b2f9b833c984499f51372e0855f6b3b229679d4b
rohithshetty7 <rohithshetty248@gmail.com>
2024-01-14 16:18:34 +0530

    intial commit

---
+++ components/index.tsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero"
+import CustomButton from "./CustomButton"
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import CustomFilter from './CustomFilter';
+import SearchBar from './SearchBar';
+import SearchManufacurer from './SearchManufacurer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
+import ShowMore from './ShowMore';
+
+export {Hero,CustomButton,Navbar,Footer,CustomFilter,SearchBar,SearchManufacurer,CarCard,CarDetails,ShowMore}
